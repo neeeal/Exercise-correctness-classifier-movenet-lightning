95ac71ab500ac8869d7522df4b369e8417117a7e
Niil <91337648+neeeal@users.noreply.github.com>
2023-05-30 02:27:29 +0800

    Add files via upload
    can load images from local video or hardware camera

--- camera_testing.py
+++ camera_testing.py
@@ -15,14 +15,17 @@ import matplotlib.patches as patches
 import imageio
 from IPython.display import HTML, display
 
+use='squat'
+
 print('Loading Movenet Lightning pose estimation model')
 module = tf.saved_model.load('D:\\Documents\\College\\3rd Year\\2nd Sem\\CPE 020\\final_proj\\movenet\\lightning')
 model = module.signatures['serving_default']
 input_size = 192
 
 print('Loading NN classifier model')
-model_path='D:\\Documents\\College\\3rd Year\\2nd Sem\\CPE 020\\final_proj\\models\\good_models\\models_v14\\legraise.h5'
+model_path=f'D:\\Documents\\College\\3rd Year\\2nd Sem\\CPE 020\\final_proj\\models\\good_models\\models_v14\\{use}.h5'
 classifier = tf.keras.models.load_model(model_path)
+classifier.summary()
 
 def movenet(input_image):
     """Runs detection on an input image.
@@ -47,7 +50,10 @@ def movenet(input_image):
 # Open the device at the ID 0
 # Use the camera ID based on
 # /dev/videoID needed
-cap = cv2.VideoCapture(1)
+vid_path = f'D:\\Documents\\College\\3rd Year\\2nd Sem\\CPE 020\\final_proj\\test_set\\{use}\\good.mp4'
+
+cap = cv2.VideoCapture(vid_path)
+##cap = cv2.VideoCapture(1)
 
 #Check if camera was opened correctly
 if not (cap.isOpened()):
@@ -55,14 +61,15 @@ if not (cap.isOpened()):
 else:
     print("Camera opened")
 
-cam_size = 720
+cam_size = 360
 #Set the resolution
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, input_size)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, input_size)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, cam_size)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, cam_size)
 
 # Capture frame-by-frame
 scores = []
 remarks = ['Very Bad','Bad','Good','Very Good']
+i=0
 while(True):
     ret, frame = cap.read()
 
@@ -71,8 +78,9 @@ while(True):
     cv2.imshow("preview",frame)
     
     cv2.imwrite("outputImage.jpg", frame)
-
-##    image = tf.image.decode_jpeg(frame)
+    # Getting only every 5th frame
+##    if i >= 5:
+    ##    image = tf.image.decode_jpeg(frame)
 
     # Resize and pad the image to keep the aspect ratio and fit the expected size.
     input_image = tf.expand_dims(frame, axis=0)
@@ -85,7 +93,7 @@ while(True):
     # Evaluate scores every 15 frames
     score = np.array(output)[0][0]
     scores.append(score)
-    if len(scores)==20:
+    if len(scores)==30:
         ave_score = sum(scores)/len(scores)
 ##        if ave_score < 0.25:
 ##            remark = remarks[0]
@@ -102,9 +110,15 @@ while(True):
         confidence = ave_score*100    
         print(f'Correctness = {confidence:.4f}          Remark = {remark}')
         scores=[]
+    i=0
+##    else:
+##        i+=1
+##        pass
 
     #Waits for a user input to quit the application 
     if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    if ret == False:
         break
 
 # When everything done, release the capture
